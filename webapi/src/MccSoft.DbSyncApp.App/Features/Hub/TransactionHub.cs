42fa2a0d22b588da018f323376dea0b47bc2227a
Kirill Tyunderov <kirill.tyunderov@mcc-tomsk.de>
2022-05-06 23:14:56 +0700

    Added sending unsynced transactions to client on connected

--- webapi/src/MccSoft.DbSyncApp.App/Features/Hub/TransactionHub.cs
+++ webapi/src/MccSoft.DbSyncApp.App/Features/Hub/TransactionHub.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -8,9 +9,12 @@ using MccSoft.DbSyncApp.App.Features.DbScheme.Dto;
 using MccSoft.DbSyncApp.Domain;
 using MccSoft.DbSyncApp.Persistence;
 using MccSoft.PersistenceHelpers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
 
 namespace MccSoft.DbSyncApp.App.Features.Hub;
 
@@ -26,12 +30,135 @@ public class TransactionHub : Microsoft.AspNetCore.SignalR.Hub
         _logger = logger;
     }
 
+    public override async Task OnConnectedAsync()
+    {
+        HttpContext? httpContext = Context.GetHttpContext();
+
+        if (httpContext != null)
+        {
+            StringValues queryValues = httpContext.Request.Query["lastSyncTransactionId"];
+            string? lastSyncTransactionId = queryValues.FirstOrDefault();
+
+            var lastSyncTransaction = await _dbContext.Transactions.GetOneOrDefault(
+                ISyncableEntity.HasId<Transaction>(lastSyncTransactionId)
+            );
+
+            IQueryable<Transaction> query = _dbContext.Transactions;
+
+            if (!string.IsNullOrEmpty(lastSyncTransactionId) && lastSyncTransaction != null)
+            {
+                query = query.Where(
+                    x => x.SyncDate >= lastSyncTransaction.SyncDate && x.Id != lastSyncTransactionId
+                );
+            }
+
+            List<TransactionDto> transactionsToBeSynced = query
+                .Select(
+                    x =>
+                        new TransactionDto()
+                        {
+                            Id = x.Id,
+                            Changes = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(
+                                x.Changes.ToString()
+                            ),
+                            ChangeType = x.ChangeType,
+                            InstanceId = x.InstanceId,
+                            CreationDate = x.CreationDate,
+                            SyncDate = x.SyncDate,
+                            TableName = x.TableName,
+                            AssemblyName = "",
+                            EntityFullName = "",
+                        }
+                )
+                .ToList();
+
+            await Clients.Caller.SendAsync("client-received", transactionsToBeSynced);
+        }
+
+        await base.OnConnectedAsync();
+    }
+
     public async Task<string> Send(string message)
     {
         await Clients.All.SendAsync("test", message);
 
         return $"{message} Processed";
     }
+
+    private static int _counter = 1;
+    public async Task SendFakeTransactions()
+    {
+        var dateTime = DateTime.Now.ToUniversalTime();
+        dateTime = dateTime.AddMilliseconds(-dateTime.Millisecond).AddMilliseconds(212);
+
+        var list = new List<TransactionDto>
+        {
+            // new TransactionDto()
+            // {
+            //     Id = $"fake-{_counter}",
+            //     Changes = new Dictionary<string, dynamic>()
+            //     {
+            //         { "Id", $"fake-sale-{_counter}" },
+            //         { "DateTime", DateTime.Now.ToUniversalTime().AddDays(_counter).ToJsonIso8601() }
+            //     },
+            //     AssemblyName =
+            //         "MccSoft.DbSyncApp.Domain, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+            //     EntityFullName = "MccSoft.DbSyncApp.Domain.Sale",
+            //     ChangeType = ChangeType.Insert,
+            //     CreationDate = DateTime.Now,
+            //     InstanceId = $"fake-sale-{_counter}",
+            //     TableName = "Sales",
+            //     SyncDate = DateTime.Now,
+            // },
+            // new TransactionDto()
+            // {
+            //     Id = $"fake-box-tr-{_counter}",
+            //     Changes = new Dictionary<string, dynamic>() { { "IsFull", false } },
+            //     AssemblyName =
+            //         "MccSoft.DbSyncApp.Domain, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+            //     EntityFullName = "MccSoft.DbSyncApp.Domain.Box",
+            //     ChangeType = ChangeType.Update,
+            //     CreationDate = DateTime.Now,
+            //     InstanceId = $"b7168640-098a-420f-9dcf-24eec036ce4f",
+            //     TableName = "Boxes",
+            //     SyncDate = DateTime.Now,
+            // },
+            // new TransactionDto()
+            // {
+            //     Id = $"fake-sale-tr-{_counter}",
+            //     Changes = new Dictionary<string, dynamic>()
+            //     {
+            //         { "DateTime", $"2022-05-03T18:{_counter % 60}:00.000Z" },
+            //         { "OptionalDateTime", $"2022-05-03T18:{_counter % 60}:01.111Z" },
+            //     },
+            //     AssemblyName =
+            //         "MccSoft.DbSyncApp.Domain, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+            //     EntityFullName = "MccSoft.DbSyncApp.Domain.Sale",
+            //     ChangeType = ChangeType.Update,
+            //     CreationDate = DateTime.Now,
+            //     InstanceId = $"149503c1-98c7-49da-b129-f157ca32c410",
+            //     TableName = "Sales",
+            //     SyncDate = DateTime.Now,
+            // },
+            new TransactionDto()
+            {
+                Id = $"fake-sale-tr-{_counter}",
+                Changes = new Dictionary<string, dynamic>(),
+                AssemblyName =
+                    "MccSoft.DbSyncApp.Domain, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+                EntityFullName = "MccSoft.DbSyncApp.Domain.Sale",
+                ChangeType = ChangeType.Delete,
+                CreationDate = DateTime.Now,
+                InstanceId = $"4ddf4e37-5dca-407c-9637-357f5fc17d5a",
+                TableName = "Sales",
+                SyncDate = DateTime.Now,
+            }
+        };
+
+        _counter += 1;
+        await Clients.All.SendAsync("client-received", list);
+    }
+
     // private static Dictionary<string, >
     public async Task<List<string>> SyncTransactions(List<TransactionDto> transactions)
     {
